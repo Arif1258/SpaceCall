725889f1142100a3639d905dcaf923ec596fd8c8
Sk Arif Ahmed <skarifahmed@Sks-MacBook-Air.local>
2025-07-21 22:31:19 +0530

    c

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,27 +1,39 @@
-import express, { urlencoded } from "express";
-import {createServer} from "node:http";
-import {Server} from "socket.io";
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
 import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
 import cors from "cors";
-import {connectToSocket} from "./controller/socketManager.js";
-import userRoutes from "./routes/user.routes.js";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
 
-const app=express();
-const server=createServer(app);
-app.set("port",(process.env.PORT || 8000));
+app.set("port", (process.env.PORT || 8000))
 app.use(cors());
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit:"40kb",extended:true}));
-app.use("/api/v1/users",userRoutes);
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
+app.use("/api/v1/users", userRoutes);
 
-const start=async ()=>{
+const start = async () => {
     app.set("mongo_user")
-        const connectionDb=mongoose.connect("mongodb+srv://arifahmedma490:12588@zoomcluster.s0rgfo0.mongodb.net/")
-        console.log("database connected");
-        server.listen(app.get("port"),()=>{
-            console.log("server is running on port 8000")
-    })
+    const connectionDb = await mongoose.connect("mongodb+srv://arifahmedma490:12588@zoomcluster.s0rgfo0.mongodb.net/")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
 }
+
+
+
 start();
